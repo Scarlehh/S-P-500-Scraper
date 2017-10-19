60fb0e699f2bad566a8e01095efa33be83a73d8d
Scarlett <gourleys@tcd.ie>
2017-10-19 21:47:03 +0100

    Add description of columns

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 
 source venv/bin/activate
 echo "Running scraper..."
-python3 scraper.py > stocksplit
+#python3 scraper.py > stocksplit
 echo "Scraper finished"
 echo "Formatting data..."
 sum=$(awk -F ' ' '{SUM = SUM + $2} END {print SUM}' stocksplit)
+echo -e "\nSymbol\tAmount"
+echo -e "------\t-------"
 awk -F ' ' "{b=\$1"'"'":\t"'"'"(((\$2 / $sum)*50000)/\$3); print b}" stocksplit
-echo "Finished! Enjoy :)"
+echo -e "\nFinished! Enjoy :)"
